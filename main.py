ad1efccf36e0e763ad80d613febfcb70747838fe
AKupriichuk <alexa05kup@gmail.com>
2025-10-07 16:55:29 +0300

    Commit 2

--- main.py
+++ main.py
@@ -4,31 +4,39 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open("players.json", 'r') as f:
+    with open("players.json", "r") as f:
         players_data = json.loads(f.read())
 
     for player_info in players_data:
-        race_data = player_info.get('race')
+        race_data = player_info.get("race")
         race_obj = None
         if race_data:
             race_obj, _ = Race.objects.get_or_create(
-                name=race_data['name'],
-                defaults={"description": race_data.get('description', '')},
+                name=race_data["name"],
+                defaults={"description": race_data.get("description", "")},
+            )
+            
+        skill_data = player_info.get("skill")
+        skill_obj = None
+        if skill_data:
+            skill_obj, _ = Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"description": skill_data.get("description", "")},
             )
 
         guild_data = player_info.get("guild")
         guild_obj = None
         if guild_data:
             guild_obj, _ = Guild.objects.get_or_create(
-                name=guild_data['name'],
-                defaults={"description": guild_data.get('description')},
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description")},
             )
 
         Player.objects.get_or_create(
-            nickname=player_info['nickname'],
+            nickname=player_info["nickname"],
             defaults={
-                "email": player_info['email'],
-                "bio": player_info['bio'],
+                "email": player_info["email"],
+                "bio": player_info["bio"],
                 "race": race_obj,
                 "guild": guild_obj,
             },
